8a2f78ad1d5cea91d2cf61656e8cc56b1c6f71e3
paxblueribbon <veganbikepunk@gmail.com>
2024-11-03 19:48:05 -0800

    Updates now redirect back to the list page

--- src/services/prison-network-service.js
+++ src/services/prison-network-service.js
@@ -4,6 +4,12 @@ class PrisonNetworkService {
   getAll() {
     return http.get('/prison/prisons', {})
 }
+
+async updateOne(params){
+  const response = await http.put('/prison/prison', params);
+  return response;
+}
+
 async getOne(pid) {
   const response = await http.get(`/prison/prison?id=${pid}`, {});
   return response
--- src/services/rule-network-service.js
+++ src/services/rule-network-service.js
@@ -4,6 +4,12 @@ class RuleNetworkService {
   getAll() {
       return http.get('/rule/rules', {})
   }
+
+  async updateOne(params){
+    const response = await http.put('/rule/rule', params);
+    return response;
+  }
+  
   async getOne(pid) {
     const response = await http.get(`/rule/rule?id=${pid}`, {});
     return response
