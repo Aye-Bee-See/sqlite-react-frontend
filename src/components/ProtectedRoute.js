ad35c3cde4145229fa19d2919cfaa588d4e7c59d
paxblueribbon <veganbikepunk@gmail.com>
2024-11-22 14:57:49 -0800

    New login component stores token in localstorage.  Created ProtectedRoute functional component.

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+  const isAuthenticated = () => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (!token) {
+      return false;
+    }
+
+    const expiration = new Date(token.expiration);
+    const now = new Date();
+
+    if (now > expiration) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      return false;
+    }
+
+    return true;
+  };
+
+  return isAuthenticated() ? Component : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
