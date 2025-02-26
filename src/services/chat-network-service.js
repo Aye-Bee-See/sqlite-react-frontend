87c3a943ed879a9159e857ffebd9ed10427d3881
paxblueribbon <veganbikepunk@gmail.com>
2025-02-25 18:30:50 -0800

    more prettier

--- src/services/chat-network-service.js
+++ src/services/chat-network-service.js
@@ -4,25 +4,37 @@ class ChatNetworkService {
 	// Create chat
 	async addOne(params, token) {
 		const response = await http.post('/chat/chat', params, {
-			headers: { Authorization: `Bearer ${token}` }
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
 		});
 		return response;
 	}
 
 	// Read all chats
 	async getAll(token) {
-		return http.get('/chat/chats', { headers: { Authorization: `Bearer ${token}` } });
+		return http.get('/chat/chats', {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
 	}
 
 	// Read one chat
 	async getOne(id, token) {
-		return http.get(`/chat/chat?id=${id}`, { headers: { Authorization: `Bearer ${token}` } });
+		return http.get(`/chat/chat?id=${id}`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
 	}
 
 	// Update one chat
 	async updateOne(params, token) {
 		const response = await http.put('/chat/chat', params, {
-			headers: { Authorization: `Bearer ${token}` }
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
 		});
 		return response;
 	}
@@ -30,7 +42,9 @@ class ChatNetworkService {
 	// Delete one chat
 	async deleteOne(id, token) {
 		const response = await http.delete('/chat/chat', {
-			headers: { Authorization: `Bearer ${token}` },
+			headers: {
+				Authorization: `Bearer ${token}`
+			},
 			data: { id }
 		});
 		return response;
@@ -40,11 +54,15 @@ class ChatNetworkService {
 	async getChatsByUserOrPrisoner(user, prisoner, token) {
 		if (user) {
 			return http.get(`/chat/chats/?user=${user}`, {
-				headers: { Authorization: `Bearer ${token}` }
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
 			});
 		} else if (prisoner) {
 			return http.get(`/chat/chats/?prisoner=${prisoner}`, {
-				headers: { Authorization: `Bearer ${token}` }
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
 			});
 		} else {
 			console.log('no user or prisoner provided');
