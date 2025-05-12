1ec6628712aeaf40bd6f599e2e929acfefb64ab2
paxblueribbon <veganbikepunk@gmail.com>
2025-05-12 14:48:36 -0700

    fix typo in multipart http

--- src/http-multipart.js
+++ src/http-multipart.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
 	baseURL: 'http://localhost:3000/',
-	headers: { 'Content-type': 'multipart/form' }
+	headers: { 'Content-type': 'multipart/form-data' }
 });
