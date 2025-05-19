01faadef0acb59609bd41adf5f1adc96b6fff28e
paxblueribbon <veganbikepunk@gmail.com>
2025-05-18 20:52:24 -0700

    Adjust network services to allow for page limits and numbers in network calls

--- src/services/rule-network-service.js
+++ src/services/rule-network-service.js
@@ -1,8 +1,10 @@
 import http from '../http-common';
 
 class RuleNetworkService {
-	getAll(token) {
-		return http.get('/rule/rules', { headers: { Authorization: `Bearer ${token}` } });
+	getAll(token, page_size = 10, page = 1) {
+		return http.get(`/rule/rules?page_size=${page_size}&page=${page}`, {
+			headers: { Authorization: `Bearer ${token}` }
+		});
 	}
 	async addOne(params, token) {
 		const response = await http.post('/rule/rule', params, {
